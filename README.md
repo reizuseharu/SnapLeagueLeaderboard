7a686e35a432ad840e07a0e40f267c1724d6381b
Reizu Seharu <reizu.seharu@gmail.com>
2020-04-12 22:32:33 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# SnapLeagueLeaderboard
-Snap League Leaderboard prototype
---
+++ precache-manifest.2b6f7faf4eef5ef785011cd5798e5861.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358fc09f7c559118d8914d1d59e31b8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b9589a8a2e7f2229a1",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "01f7a000577ac7707521",
+    "url": "/static/css/main.65b781ac.chunk.css"
+  },
+  {
+    "revision": "41b9589a8a2e7f2229a1",
+    "url": "/static/js/2.715ea608.chunk.js"
+  },
+  {
+    "revision": "75731ebd6d677f69aa1036f53d4f0c02",
+    "url": "/static/js/2.715ea608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f7a000577ac7707521",
+    "url": "/static/js/main.a40f8059.chunk.js"
+  },
+  {
+    "revision": "57e9823257d7190b45a3",
+    "url": "/static/js/runtime-main.b026eb6f.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
